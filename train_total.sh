ce1f187e3a1c685632c1ddced1f59a582cafcbb4
Olivialyt <120729537+Olivialyt@users.noreply.github.com>
2025-06-16 14:49:03 +0800

    Update train_total.sh

--- train_total.sh
+++ train_total.sh
@@ -27,3 +27,13 @@ CUDA_VISIBLE_DEVICES=0,1 python -u train_total.py \
 --data_dir_depth ./dataset/BASICS_maps/6view_depth \
 --num_epochs 50 \
 >> logs/total.log
+
+CUDA_VISIBLE_DEVICES=0,1 python -u train_total.py \
+--learning_rate 0.000004 \
+--batch_size 16 \
+--database WPC \
+--img_length_read 6 \
+--data_dir_color ./dataset/WPC_maps/6view \
+--data_dir_depth ./dataset/WPC_maps/6view_depth \
+--num_epochs 50 \
+>> logs/total.log
